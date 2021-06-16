69ffe79222e38f53afaeff1cc2b57f6065a58d30
hamstergeek <hamstergeek38@gmail.com>
2021-06-16 10:10:31 +0700

    create template dog and breed

---
+++ src/components/BreedList.jsx
@@ -0,0 +1,7 @@
+const BreedList = () => {
+
+
+}
+
+
+export default BreedList
---
+++ src/components/DogCardInfo.jsx
@@ -0,0 +1,22 @@
+const DogCardInfo = ({ imgUrl, pictureId }) => {
+  return (
+    <div className="card card-dog">
+      <div className="card-image">
+        <figure className="image" style={{ backgroundImage: `url(${imgUrl})` }}>
+          <img
+            src={imgUrl}
+            alt={`${imgUrl} a nice dog`}
+            className="is-sr-only"
+          />
+        </figure>
+      </div>
+      <div className="card-content">
+        <div className="content">
+          <strong>Picture Id:</strong> {pictureId}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default DogCardInfo
